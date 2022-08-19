0fbb523be717401fa0974720d222e9ecebc2cc6f
dileepyadavns <dileepyadavneo@gmail.com>
2022-08-19 19:15:44 +0530

    building recommendation system

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-import numpy as np
-import pandas as pd
-from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import json
-import bs4 as bs
-import urllib.request
-import pickle
-import requests
+
 # load the nlp model and tfidf vectorizer from disk
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
@@ -23,7 +14,6 @@ def create_similarity():
     return data,similarity
 
 def rcmd(m):
-    
     m = m.lower()
     try:
         data.head()
@@ -42,3 +32,39 @@ def rcmd(m):
             a = lst[i][0]
             l.append(data['movie_title'][a])
         return l
+
+
+def get_suggestions():
+    data = pd.read_csv('main_data.csv')
+    return list(data['movie_title'].str.capitalize())
+
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    suggestions = get_suggestions()
+    return render_template('home.html',suggestions=suggestions)
+
+
+
+
+
+
+@app.route("/similarity",methods=["POST"])
+def similarity():
+    movie = request.form['name']
+    rc = rcmd(movie)
+    if type(rc)==type('string'):
+        return rc
+    else:
+        m_str="---".join(rc)
+        return m_str
