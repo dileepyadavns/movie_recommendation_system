2a301a8b7c1815f8da14fb2111c88df59b46e442
dileepyadavns <dileepyadavneo@gmail.com>
2022-09-06 12:05:46 +0530

    building recommendation system

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import pandas as pd
 from flask import Flask, render_template, request
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import json
+
 import bs4 as bs
-import urllib.request
-import pickle
-import requests
+
 
 
 
